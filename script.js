bcf9be0704740fb618bde2bfe09c542e444e426b
Vincent Tai <177089640+Liyang-86@users.noreply.github.com>
2024-09-16 21:40:09 +0800

    add decreasing opacity and rgb color

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function createGrid(size) {
             const sqr = document.createElement("div");
             sqr.classList.add("sqr");
             sqr.addEventListener("mouseenter", (event) => {
-                mouseEnter(event.target);
+                mouseEnter(event.target, getRandomColor());
             });
             sqr.addEventListener("mouseleave", (event) => {
                 mouseLeave(event.target);
@@ -30,28 +30,47 @@ function createGrid(size) {
 
 createGrid(16);
 
-// for (let i = 0; i < 16; i++) {
-//     const row = document.createElement("div");
-//     row.classList.add("row");
-//     container.appendChild(row);
-//     for (let j = 0; j < 16; j++) {
-//         const sqr = document.createElement("div");
-//         sqr.classList.add("sqr");
-//         // sqr.innerHTML = (i + 1) + "x" + (j + 1);
-//         sqr.addEventListener("mouseenter", (event) => {
-//             mouseEnter(event.target);
-//         });
-//         sqr.addEventListener("mouseleave", (event) => {
-//             mouseLeave(event.target);
-//         });
-//         row.appendChild(sqr);
-//     }
-// }
+function getRandomColor() {
+    let colors = [
+        '#ff0000', // Red
+        '#00ff00', // Lime
+        '#0000ff', // Blue
+        '#ffff00', // Yellow
+        '#ff00ff', // Magenta
+        '#00ffff', // Cyan
+        '#000000', // Black
+        '#ffffff', // White
+        '#808080', // Gray
+        '#800000', // Maroon
+        '#808000', // Olive
+        '#008000', // Green
+        '#800080', // Purple
+        '#008080', // Teal
+        '#ff4500', // OrangeRed
+        '#da70d6', // Orchid
+        '#87ceeb', // SkyBlue
+        '#4682b4', // SteelBlue
+        '#d2691e', // Chocolate
+        '#ff6347', // Tomato
+        '#2e8b57', // SeaGreen
+        '#6a5acd', // SlateBlue
+        '#ffd700', // Gold
+        '#ff1493', // DeepPink
+        '#40e0d0'  // Turquoise
+    ];
+    
+    let randomColor = colors[Math.floor(Math.random() * colors.length)];
+    return randomColor;
+}
 
-function mouseEnter(e) {
-    e.classList.add("light");
+function mouseEnter(e, color) {
+    let newOpacity = Math.min(parseFloat(window.getComputedStyle(e).opacity) + 0.1, 1);
+    e.style.opacity = newOpacity;
+    e.style.background = color;
+    // e.classList.add("light");
 }
 
 function mouseLeave(e) {
-    e.classList.remove("light");
+    e.style.background = "grey";
+    // e.classList.remove("light");
 }
